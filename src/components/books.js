d13f864037d0c5c09d61fd9d557104d8dcb77018
tushar R <tusharrravindran@gmail.com>
2023-01-18 22:27:15 +0530

    axion for API_URL

---
+++ src/components/books.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Books(props) {
+  return ( 
+  <div>
+      <h1>From Api data</h1>
+      {props.books.map((book) => {
+        return (
+        <div key={book.id}>
+        <h2>{book.title}</h2>
+        <p>{book.body}</p>
+    </div>
+  );
+})}
+</div>
+  );
+}
+
+
+export default Books
